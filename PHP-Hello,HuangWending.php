ac5c21b2513c7e9321caf8e6d3c4ff603588698e
黃(黄)文定-ㄏㄨㄤˊ ㄨㄣˊㄉㄧㄥˋ <91007091+HuangWending@users.noreply.github.com>
2023-06-01 12:01:58 +0800

    Add files via upload

---
+++ PHP-Hello,HuangWending.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello, HuangWending</title>
+</head>
+<body>
+    <?php
+        $name = "HuangWending";
+        echo "Hello, " . $name;
+    ?>
+</body>
+</html>
